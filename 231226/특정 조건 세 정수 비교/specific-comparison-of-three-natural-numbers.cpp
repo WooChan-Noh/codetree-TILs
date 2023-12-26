1cbefa372c755e5d4ba28a7c609366c550fb1b3e
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-26 17:55:54 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231226/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 231226/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -9,10 +9,10 @@ int main() {
     else 
         cout<<0<<" ";
 
-    if(a==b && b==c)
-        cout<<1<<" ";
+    if((a==b) && (b==c))
+        cout<<1;
     else    
-        cout<<0<<" ";
+        cout<<0;
     // 여기에 코드를 작성해주세요.
     return 0;
 }

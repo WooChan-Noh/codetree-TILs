89d9274620b743a386a9cebe1daf0074d25550de
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-26 17:48:36 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231226/점수 비교/score-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int aEnglish,aMath,bEnglish,bMath;
+    cin>>aMath>>aEnglish>>bMath>>bEnglish;
+
+    if((aEnglish>bEnglish)&&(aMath>>bMath))
+        cout<<1;
+    else
+        cout<<0;
+
+    return 0;
+}

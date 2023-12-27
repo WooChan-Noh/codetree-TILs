8804ce1fc22c4f01434f84ed3e66dd9a3345819b
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-27 11:54:04 +0900

    Title: 증가시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int temp=10;
+    while(true)
+    {
+    
+        cout<<temp<<" ";
+        temp++;
+        if(temp>26)
+            break;
+    }
+    return 0;
+}

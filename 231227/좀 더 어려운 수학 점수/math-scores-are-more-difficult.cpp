01ccf6db2dc55458a133b2ef4e123bea34dd4d36
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-27 10:53:44 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231227/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int aMathScore,aEnglishScore,bMathScore,bEnglishScore;
+    cin>>aMathScore>>aEnglishScore>>bMathScore>>bEnglishScore;
+
+    if(aMathScore>=bMathScore && aEnglishScore>bEnglishScore)
+    {
+        cout<<'A';
+    }
+    else
+    {
+        cout<<'B';
+    }
+    return 0;
+}

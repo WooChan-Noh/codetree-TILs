d2cbdcb1172cb9a194a436530a6cab7a4cce7642
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-27 11:55:23 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,temp=1;
+    cin>>n;
+
+    while(temp<=n)
+    {
+        cout<<temp<<" ";
+        temp++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

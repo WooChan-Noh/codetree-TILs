30787f1fb4b6792412c5d3c02972cdfe4d8d6c5b
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-27 12:02:06 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int temp=26;
+    // 여기에 코드를 작성해주세요.
+    while(9<temp)
+    {
+        cout<<temp<<" ";
+        temp--;
+    }
+    return 0;
+}

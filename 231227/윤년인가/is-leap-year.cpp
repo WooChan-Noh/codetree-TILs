5fcbf9c81cfe50c9f2bbc00c2f6e15b7b9cc2fdd
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-27 11:11:41 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231227/윤년인가/is-leap-year.cpp
+++ 231227/윤년인가/is-leap-year.cpp
@@ -5,7 +5,7 @@ int main() {
     int year;
     cin>>year;
 
-    if(year%4==0 ||  year%400==0 && year%100==0) 
+    if(year%4==0 ||  (year>=400 && year%400==0)) 
         cout<<"true";
     else
         cout<<"false";

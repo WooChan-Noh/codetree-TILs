f383b29d95d6ce6a9b126e56e6780d023328e94c
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-28 17:21:05 +0900

    Title: 출석 부르기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231228/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    while(true)
+    {
+        int n;
+        cin>>n;
+        if(n==1)
+            cout<<"Jhon"<<endl;
+        else if(n==2)
+            cout<<"Tom"<<endl;
+        else if(n==3)
+            cout<<"Paul"<<endl;
+        else if(n==4)
+            cout<<"Sam"<<endl;
+        else   
+        {
+            cout<<"Vacancy";
+            break;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

4a181b65252ec1dfa02b185b88d37f88f91d0b61
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-28 17:30:19 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231228/3n + 1 수열/3n-plus-1-sequence.cpp
+++ 231228/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -5,6 +5,11 @@ int main() {
     cin>>n;
     while(true)
     {
+          if(n==1)
+        {
+            cout<<count;
+            break;
+        }
         if(n%2==0)
         {
             n/=2;
@@ -15,11 +20,7 @@ int main() {
             n=n*3+1;
             count++;
         }
-        if(n==1)
-        {
-            cout<<count;
-            break;
-        }
+      
     }
     // 여기에 코드를 작성해주세요.
     return 0;

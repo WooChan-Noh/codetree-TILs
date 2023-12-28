1bda6fd5cb3d1918c9e39d67602d5137b9adc623
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-28 11:02:47 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin>>a>>b;
+    for(int i=a;i<=b;)
+    {
+        cout<<i<<" ";
+        if(i%2==0)
+            i+=3;
+        else
+            i*=2;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

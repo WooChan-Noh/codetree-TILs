dd5c2aee6a97a0d2460818a0a7eced6c6aa3c6e5
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-28 17:57:17 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<'C';
+            return 0;
+        }
+    }
+    cout<<'P';
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

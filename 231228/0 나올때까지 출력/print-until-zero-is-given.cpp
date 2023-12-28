d274d823199144c74c1ce7dbd8cbe0e3153982d2
WooChan-Noh <woochan.noh99@gmail.com>
2023-12-28 17:16:36 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    while(true)
+    {
+        int n;
+        cin>>n;
+        if(n==0)
+            break;
+        cout<<n<<endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
